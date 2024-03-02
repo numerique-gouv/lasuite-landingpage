b773363831705c801fce850ea32575cae864ec99
Lebaud Antoine <lebaud.antoine131@gmail.com>
2024-03-02 23:57:59 +0100

    ✨ (frontend) set up Matomo analytics
    By default, Matomo tracking logic was added to the `_app`
    component.
    
    It pushes a new event to the data layer, and adds a script
    in the Head html element.
    
    Code is strongly inspired by the integration guide, which
    suggest copy-pasting a code snippet for react user.
    
    Done:
    - Update legal terms
    - Allow user to deactivate Matomo tracking using an Iframe

--- src/constant.ts
+++ src/constant.ts
@@ -3,6 +3,7 @@ export const ADDRESS_DINUM = "DINUM, 20 avenue de Ségur 75007 Paris";
 
 export const URL_SITE = "lasuite.numerique.gouv.fr";
 export const TITLE_SITE = "La Suite numérique";
+export const MATOMO_ID = "https://stats.data.gouv.fr/js/container_h2RIEY3C.js";
 
 export const NEWSLETTER_FORM =
   "https://4f0df3d6.sibforms.com/serve/MUIFAKrbltNDRgRE9y7Ijv8nF1SN65VwHtbGH33ZGje0x0-VS9Rr8bbD3LefzRQE0Eu3KtRtSBbaDueFVEARVVYp2Bb3JLj9LBk9SNK1XX2rSgs9adgkR67Xhi1RUqGU7wcVv2U42hHq7IIyH3N3Eu82Ci39-aYzr2HUoNERYKoTbYfeINulmU8Y-BX6jaK5QNlZg6wyBhMl52sq";
--- src/pages/suivi/index.tsx
+++ src/pages/suivi/index.tsx
@@ -1,5 +1,8 @@
 import { Layout, ContentSection } from "@/components/Layout";
 import { SubTitle, Header, Text } from "@/components/LegalTerms";
+import { ExternalLink } from "@/components/Core";
+import React from "react";
+import { URL_SITE } from "@/constant";
 
 export default function Suivi() {
   return (
@@ -8,7 +11,22 @@ export default function Suivi() {
       <ContentSection classNameChildren="items-start max-w-[54rem]">
         <div>
           <SubTitle>Cookies déposés</SubTitle>
-          <p className="mb-3">Nous ne déposons aucun cookie.</p>
+          <Text>
+            Ce site dépose un petit fichier texte (un « cookie ») sur votre
+            ordinateur lorsque vous le consultez. Cela nous permet de mesurer le
+            nombre de visites et de comprendre quelles sont les pages les plus
+            consultées.
+          </Text>
+          <Text className="font-serif">
+            Vous pouvez vous opposer au suivi de votre navigation sur ce site
+            web. Cela protégera votre vie privée, mais empêchera également le
+            propriétaire d&apos;apprendre de vos actions et de créer une
+            meilleure expérience pour vous et les autres utilisateurs.
+          </Text>
+          <iframe
+            className="w-full"
+            src="https://stats.data.gouv.fr/index.php?module=CoreAdminHome&amp;action=optOut&amp;language=fr"
+          ></iframe>
         </div>
         <div>
           <SubTitle>
@@ -27,7 +45,31 @@ export default function Suivi() {
             sont exemptés d’autorisation préalable.
           </Text>
           <p className="mb-3">
-            Nous n’utilisons aucun outil pour surveiller les usages du site.
+            Nous utilisons pour cela{" "}
+            <ExternalLink
+              href="https://matomo.org/"
+              className="underline underline-offset-4"
+            >
+              Matomo
+            </ExternalLink>
+            , un outil{" "}
+            <ExternalLink
+              href="https://matomo.org/free-software/"
+              className="underline underline-offset-4"
+            >
+              libre
+            </ExternalLink>
+            , paramétré pour être en conformité avec la{" "}
+            <ExternalLink
+              href="https://www.cnil.fr/fr/cookies-et-autres-traceurs/regles/cookies-solutions-pour-les-outils-de-mesure-daudience"
+              className="underline underline-offset-4"
+            >
+              recommandation « Cookies »
+            </ExternalLink>{" "}
+            de la <span className="decoration-dotted underline">CNIL</span>.
+            Cela signifie que votre adresse IP, par exemple, est anonymisée
+            avant d’être enregistrée. Il est donc impossible d’associer vos
+            visites sur ce site à votre personne.
           </p>
         </div>
         <div>
@@ -35,8 +77,15 @@ export default function Suivi() {
             Je contribue à enrichir vos données, puis-je y accéder ?
           </SubTitle>
           <Text>
-            Les statistisques d&apos;usage de nos produits ne sont pas encore en
-            accès libre.
+            Bien sûr ! Les statistiques d’usage de la majorité de nos produits,
+            dont {URL_SITE}, sont disponibles en accès libre sur{" "}
+            <ExternalLink
+              href="https://stats.data.gouv.fr/index.php?module=CoreHome&action=index&idSite=21&period=range&date=previous30#?period=range&date=previous30&module=VisitsSummary&action=index&idSite=21&category=Dashboard_Dashboard&subcategory=1"
+              className="underline underline-offset-4"
+            >
+              stats.data.gouv.fr
+            </ExternalLink>{" "}
+            .
           </Text>
         </div>
       </ContentSection>
