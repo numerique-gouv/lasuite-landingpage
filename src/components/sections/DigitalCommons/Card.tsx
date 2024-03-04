d463702a84abdf0bdb5d4ee6c1edc19a57a396ad
Emmanuel Pelletier <manu@habite.la>
2024-03-04 11:25:37 +0100

    ♻️(react) destructure params now we dont pass down props anymore

--- src/components/sections/DigitalCommons/Card.tsx
+++ src/components/sections/DigitalCommons/Card.tsx
@@ -7,15 +7,15 @@ export interface CardProps {
   img: string;
 }
 
-export const Card: React.FC<CardProps> = (props) => (
+export const Card: React.FC<CardProps> = ({ img, title, href }) => (
   <div className="flex flex-col items-center">
-    <Image src={props.img} height={92} width={92} alt="" />
-    <h3 className="text-lg text-dinum-grey-1 mt-6 mb-4">{props.title}</h3>
+    <Image src={img} height={92} width={92} alt="" />
+    <h3 className="text-lg text-dinum-grey-1 mt-6 mb-4">{title}</h3>
     <div className="flex gap-1 justify-center items-center">
       <ExternalLink
         className="text-sm underline text-dinum-blue-1 external-link-blue"
-        href={props.href}
-        aria-label={`${props.title} - En savoir plus`}
+        href={href}
+        aria-label={`${title} - En savoir plus`}
       >
         En savoir plus
       </ExternalLink>
