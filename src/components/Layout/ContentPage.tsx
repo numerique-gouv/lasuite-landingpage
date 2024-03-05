5d9e8b5924522ed962072ca9842b767e12befe1e
Emmanuel Pelletier <manu@habite.la>
2024-03-05 15:42:35 +0100

    ♻️(content) use basic html for content pages instead of react components
    not such a necessary change but I feel sticking to basic HTML for
    content pages better match our use case.
    
    I guess people commiting directly for content would be more used to
    basic HTML than abstractions behind react components.
    
    Yeah I'm a little biased and I understand the counter argument :)
    
    one cool thing to do this is we don't have to actually maintain
    abstractions as the content grows
    
    Kinda nitpicking certainly!

--- src/components/Layout/ContentPage.tsx
+++ src/components/Layout/ContentPage.tsx
@@ -10,7 +10,11 @@ export const ContentPage: React.FC<{
 }> = ({ title, children }) => {
   return (
     <Layout title={title}>
-      <Header>{title}</Header>
+      <div className="py-9 bg-dsfr-blue-3">
+        <h1 className="fr-container font-bold text-[2rem] md:text-[2.5rem]">
+          {title}
+        </h1>
+      </div>
       <div className="fr-container page-content py-12">
         <div className="max-w-[50rem]">{children}</div>
       </div>
