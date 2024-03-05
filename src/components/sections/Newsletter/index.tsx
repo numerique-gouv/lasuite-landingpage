c3996a9621743404dd45a07c413818c427ac967f
Emmanuel Pelletier <manu@habite.la>
2024-03-05 14:42:41 +0100

    💄(newsletter) make newsletter block the width of header/footer
    even if the figma design is different it seems the actual goal is to
    match the newsletter block with footer, not landing page width

--- src/components/sections/Newsletter/index.tsx
+++ src/components/sections/Newsletter/index.tsx
@@ -3,7 +3,7 @@ import { NEWSLETTER_FORM } from "@/constant";
 
 export const Section = () => (
   <div className="bg-dsfr-blue-1 py-8 text-left">
-    <div className="max-w-[78rem] px-[24px] mx-auto py-8">
+    <div className="fr-container">
       <div className="flex flex-col items-start justify-between">
         <h2 className="text-[1.375rem] leading-7 font-bold mb-3">
           Recevoir la newsletter de La Suite
