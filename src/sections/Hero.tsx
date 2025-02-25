55ca8e77c42d0886156702fe60c94273e2a71bb3
Eléonore Voisin <elevoisin@gmail.com>
2025-02-25 11:15:18 +0100

    ✨(front) refonte ui V1
    add beta to products list

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -31,7 +31,7 @@ export const Hero = () => {
           </h2>
           <p className="text-center md:w-[80%] mx-auto text-greyscale-700 text-base">
             Créez. Organisez. Collaborez. Un environnement open source, conçu
-            pour le secteur public et ouvert aux communs
+            pour le secteur public et ouvert aux communs.
           </p>
         </div>
       </div>
