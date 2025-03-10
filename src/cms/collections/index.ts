d799c7b778d5e744f11be184aa7f1063c3dbdff0
lebaudantoine <lebaud.antoine131@gmail.com>
2025-03-10 13:34:54 +0100

    🔥(frontend) delete "commun" page implementation
    Remove implementation of "commun" page that was previously hidden by
    @NathanVasse months ago but remained in codebase. Page is unlikely to return
    in short term as the responsible team has changed. Cleanup improves codebase
    maintainability.
    
    I removed all CMS-related parts.

--- src/cms/collections/index.ts
+++ src/cms/collections/index.ts
@@ -1,10 +1,9 @@
 import { collection as homepageFile } from './homepage'
-import { collection as communsFile } from './communs'
 import { collection as servicePageCollection } from './service-page'
 import { collection as contentPageCollection } from './content-page'
 
 const collections = {
-  fileCollections: [homepageFile, communsFile],
+  fileCollections: [homepageFile],
   folderCollections: [servicePageCollection, contentPageCollection],
 }
 
