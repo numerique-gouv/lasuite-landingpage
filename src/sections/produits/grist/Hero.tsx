7f9986be761c6fa6f09cb6640d7ec5b36022025d
Emmanuel Pelletier <manu@habite.la>
2025-07-21 14:52:15 +0200

    🐛(grist) fix wrong letter spacing value
    I misued the letterSpacing value with % unit but it's not supported in
    every browser. use ems instead to have the same result, supported by
    everything

--- src/sections/produits/grist/Hero.tsx
+++ src/sections/produits/grist/Hero.tsx
@@ -16,7 +16,7 @@ export const Hero = () => {
         <div className="flex flex-col-reverse items-start md:items-center gap-4">
           <h1
             className="text-pretty text-greyscale-900 lg:text-5xl text-4xl sm:leading-[42px] lg:leading-[52px] font-bold md:text-center"
-            style={{ letterSpacing: '-2%' }}
+            style={{ letterSpacing: '-0.02em' }}
           >
             {t('grist-landing.hero.title')}
           </h1>
