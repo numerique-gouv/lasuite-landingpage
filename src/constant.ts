c60dd5ad7ebaa1b9200ada71123911f29e64fa90
Eléonore Voisin <elevoisin@gmail.com>
2025-11-20 11:30:17 +0100

    🔧(front) change url webinaire

--- src/constant.ts
+++ src/constant.ts
@@ -8,6 +8,9 @@ export const MATOMO_ID = 'https://stats.data.gouv.fr/js/container_h2RIEY3C.js'
 export const NEWSLETTER_FORM =
   'https://4f0df3d6.sibforms.com/serve/MUIFAKrbltNDRgRE9y7Ijv8nF1SN65VwHtbGH33ZGje0x0-VS9Rr8bbD3LefzRQE0Eu3KtRtSBbaDueFVEARVVYp2Bb3JLj9LBk9SNK1XX2rSgs9adgkR67Xhi1RUqGU7wcVv2U42hHq7IIyH3N3Eu82Ci39-aYzr2HUoNERYKoTbYfeINulmU8Y-BX6jaK5QNlZg6wyBhMl52sq'
 
+export const WEBINAIRE_URL =
+  'https://4f0df3d6.sibforms.com/serve/MUIFAM04omZ5l-BIAyEWSC8KKbXjrZ-mnM3gywzBFVt7yJJVATMb_Kct_JEuCEWaSiBnPSxUDSLkcMf-3DCTy1DHEjgVrxiY4BIZA43L5amMjiqTOpczMPvxkZfg--0pIED0XaSsrpxocCSicdRGPQGvszgHDnmaJfWaJuIDw5h0i3pDotP-XZpY-usa2LRZj6MadTh0C_sVMLHS'
+
 const allConstants = {
   CONTACT_EMAIL,
   ADDRESS_DINUM,
@@ -15,6 +18,7 @@ const allConstants = {
   TITLE_SITE,
   MATOMO_ID,
   NEWSLETTER_FORM,
+  WEBINAIRE_URL,
 }
 
 export default allConstants
