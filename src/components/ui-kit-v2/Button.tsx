numerique-gouv/lasuite-landingpage

commit 8c90055979f88e45635cef89f82872a866148505

✨(rendez-vous) added selection process for appointment booking (#172)

src/components/ui-kit-v2/Button.tsx
@@ -15,7 +15,7 @@ type ButtonVariant =
   | 'tertiary_info'
   | 'tertiary_bordered'
   | 'none'
-type ButtonSize = 'large' | 'small'
+type ButtonSize = 'large' | 'medium' | 'small'
 
 interface ButtonProps {
   children?: React.ReactNode
@@ -30,13 +30,13 @@ interface ButtonProps {
   iconPosition?: 'left' | 'right'
   className?: string
   style?: React.CSSProperties
+  onClick?: React.MouseEventHandler<HTMLButtonElement | HTMLAnchorElement>
 }
 
 const transition = 'transition ease-in-out delay-50 duration-300'
 // core sans padding/hauteur: on applique via variables CSS avec des valeurs par défaut
 const core =
   'flex rounded rounded-1 justify-center items-center text-base w-full md:w-fit text-left gap-[7px] px-4 h-10'
-
 const styles = {
   // base variants (brand by default)
   primary_brand: `${core} ${transition} font-medium text-brand-050 bg-brand-550 hover:bg-brand-650`,
@@ -58,7 +58,8 @@ const styles = {
   none: 'text-blue-1 underline underline-offset-2 hover:no-underline',
 
   large: '!text-xl md:!py-4 md:!px-8',
-  small: 'md:!py-2 md:!px-4 h-8',
+  medium: 'md:!py-2 md:!px-4 h-8',
+  small: 'md:!py-1 h-8 text-xs',
   fullWidth: '!w-full',
 }
 
@@ -74,6 +75,7 @@ export const Button: React.FC<ButtonProps> = ({
   'aria-label': ariaLabel,
   className,
   style,
+  onClick,
 }) => {
   const t = useTranslations()
   const normalizedVariant = (variant as string) || 'primary_brand'
@@ -91,6 +93,7 @@ export const Button: React.FC<ButtonProps> = ({
     {
       [styles.none]: isNone,
       [styles.large]: size === 'large' && !isNone,
+      [styles.medium]: size === 'medium' && !isNone,
       [styles.small]: size === 'small' && !isNone,
       [styles.fullWidth]: fullWidth && !isNone,
     },
@@ -113,7 +116,9 @@ export const Button: React.FC<ButtonProps> = ({
   const content = (
     <>
       {icon && iconPosition === 'left' && (
-        <span aria-hidden="true">{icon}</span>
+        <span className="flex" aria-hidden="true">
+          {icon}
+        </span>
       )}
       <span>{children}</span>
       {icon && iconPosition === 'right' && (
@@ -134,6 +139,7 @@ export const Button: React.FC<ButtonProps> = ({
         className={classes}
         aria-label={finalAriaLabel}
         style={inlineStyle}
+        onClick={onClick}
       >
         {content}
       </LinkComponent>
@@ -141,7 +147,13 @@ export const Button: React.FC<ButtonProps> = ({
   }
 
   return (
-    <button className={classes} aria-label={finalAriaLabel} style={inlineStyle}>
+    <button
+      className={classes}
+      aria-label={finalAriaLabel}
+      style={inlineStyle}
+      onClick={onClick}
+      type="button"
+    >
       {content}
     </button>
   )

src/components/NavBar.tsx
@@ -17,10 +17,12 @@ import { useTranslations } from '@/locales/useTranslations'
  * We kinda match DSFR styling here but with custom dom/classes for our specific use case.
  */
 export const NavBar = ({
+  isLogoGouv = true,
   logo,
   loginUrl,
   translation,
 }: {
+  isLogoGouv?: boolean
   logo?: React.ReactNode
   loginUrl?: string
   translation?: boolean
@@ -31,25 +33,15 @@ export const NavBar = ({
   return (
     <header className="lg:w-[70em] px-4 max-w-container w-[100%] mx-auto fade-in px-4 relative top-0 right-0 left-0 z-50">
       <div className="flex items-center">
-        <div className="flex items-center gap-4 sm:hidden">
-          {/*          <button
-            className="w-[48px] h-[48px] absolute text-brand-550 top-[9.5px] left-[19px] p-2 rounded-md focus:outline-none"
-            onClick={() => setIsOpen(!isOpen)}
-            aria-label={isOpen ? 'Fermer le menu' : 'Ouvrir le menu'}
-          >
-            {isOpen ? (
-              <CloseIcon className="text-3xl" aria-hidden="true" />
-            ) : (
-              <MenuIcon className="text-3xl" aria-hidden="true" />
-            )}
-          </button>*/}
-        </div>
+        <div className="flex items-center gap-4 sm:hidden"></div>
         <div className="relative sm:mx-0 sm:flex py-3.5 md:items-center fr-enlarge-link">
-          <Image
-            className="mr-3 w-28 hidden sm:block md:block"
-            src={LogoGouvSvg}
-            alt="GOUVERNEMENT - Liberté, Égalité, Fraternité"
-          />
+          {isLogoGouv && (
+            <Image
+              className="mr-3 w-28 hidden sm:block md:block"
+              src={LogoGouvSvg}
+              alt="GOUVERNEMENT - Liberté, Égalité, Fraternité"
+            />
+          )}
           <Link href="/" className="sm:ml-5">
             {logo || (
               <Image
@@ -71,6 +63,16 @@ export const NavBar = ({
           <div className="hidden md:block">
             {/*{translation && <LocaleSwitcher />}*/}
           </div>
+          {isLogoGouv && !loginUrl && (
+            <Button
+              className="hidden md:flex"
+              href="/rendez-vous"
+              variant=""
+              size="medium"
+            >
+              Prendre rendez-vous
+            </Button>
+          )}
           <LaGaufre />
         </div>
       </div>

src/components/content-blocks/BlockHero.tsx
@@ -5,6 +5,7 @@ import { productBanners } from '@/assets/products/banners'
 import { Button } from '@/components/ui-kit-v2/Button'
 import { SocialProof } from '@/components/content-blocks/SocialProof'
 import { useTranslations } from '@/locales/useTranslations'
+import { WEBINAIRE_URL } from '@/constant'
 
 export type ProductContent = Record<string, any>
 
@@ -50,7 +51,7 @@ export const BlockHero: React.FC<{
           <span className="text-gray-550">
             <a
               className="text-gray-550 underline underline-offset-2"
-              href="https://4f0df3d6.sibforms.com/serve/MUIFAM04omZ5l-BIAyEWSC8KKbXjrZ-mnM3gywzBFVt7yJJVATMb_Kct_JEuCEWaSiBnPSxUDSLkcMf-3DCTy1DHEjgVrxiY4BIZA43L5amMjiqTOpczMPvxkZfg--0pIED0XaSsrpxocCSicdRGPQGvszgHDnmaJfWaJuIDw5h0i3pDotP-XZpY-usa2LRZj6MadTh0C_sVMLHS"
+              href={WEBINAIRE_URL}
               target="_blank"
               aria-label={`Inscrivez-vous au prochain webinaire - ${t('common.new_window')}`}
             >

src/components/content-blocks/Decisions.tsx
@@ -60,7 +60,6 @@ export const Decisions: React.FC<DecisionsData> = ({
               <Button
                 variant={'secondary_brand'}
                 href={button.url}
-                target="_blank"
                 icon={<CalendarIcon />}
                 iconPosition="left"
               >

src/components/content-blocks/ProductsHeader.tsx
@@ -77,7 +77,7 @@ const HeaderRight: React.FC<{ links?: HeaderLink[] }> = ({ links }) => {
           <Button
             href={loginLink.url}
             variant="tertiary_brand"
-            size="small"
+            size="medium"
             icon={<LoginIcon />}
             className="!p-2 !min-w-0"
             target="_blank"
@@ -96,7 +96,7 @@ const HeaderRight: React.FC<{ links?: HeaderLink[] }> = ({ links }) => {
                   target="_blank"
                   href={link.url}
                   variant={variant}
-                  size="small"
+                  size="medium"
                 >
                   {link.title}
                 </Button>

src/components/icons/rendezvous/AppointmentIcon.tsx
@@ -1,0 +1,25 @@
+export const AppointmentIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    aria-hidden="true"
+  >
+    <path
+      d="M16 1C16.5523 1 17 1.44772 17 2V3H19C19.7957 3 20.5585 3.3163 21.1211 3.87891C21.6837 4.44151 22 5.20435 22 6V7.5C22 8.05228 21.5523 8.5 21 8.5C20.4477 8.5 20 8.05228 20 7.5V6C20 5.73478 19.8946 5.48051 19.707 5.29297C19.5195 5.10543 19.2652 5 19 5H17V6C17 6.55228 16.5523 7 16 7C15.4477 7 15 6.55228 15 6V5H9V6C9 6.55228 8.55228 7 8 7C7.44772 7 7 6.55228 7 6V5H5C4.73478 5 4.4805 5.10543 4.29297 5.29297C4.10543 5.4805 4 5.73478 4 6V9H8C8.55228 9 9 9.44771 9 10C9 10.5523 8.55228 11 8 11H4V20C4 20.2652 4.10543 20.5195 4.29297 20.707C4.48051 20.8946 4.73478 21 5 21H8.5C9.05228 21 9.5 21.4477 9.5 22C9.5 22.5523 9.05228 23 8.5 23H5C4.20435 23 3.44152 22.6837 2.87891 22.1211C2.3163 21.5585 2 20.7957 2 20V6C2 5.20435 2.3163 4.44152 2.87891 3.87891C3.44152 3.3163 4.20435 3 5 3H7V2C7 1.44772 7.44772 1 8 1C8.55228 1 9 1.44772 9 2V3H15V2C15 1.44772 15.4477 1 16 1Z"
+      fill="#EDF0FF"
+    />
+    <path
+      d="M16 13C16.5523 13 17 13.4477 17 14V15.6445L18.1299 16.3516C18.5982 16.6443 18.741 17.2621 18.4482 17.7305C18.1555 18.1986 17.5386 18.3404 17.0703 18.0479L15.4697 17.0479C15.1776 16.8651 15.0001 16.5448 15 16.2002V14C15 13.4477 15.4477 13 16 13Z"
+      fill="#EDF0FF"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M16 9C19.866 9 23 12.134 23 16C23 19.866 19.866 23 16 23C12.134 23 9 19.866 9 16C9 12.134 12.134 9 16 9ZM16 11C13.2386 11 11 13.2386 11 16C11 18.7614 13.2386 21 16 21C18.7614 21 21 18.7614 21 16C21 13.2386 18.7614 11 16 11Z"
+      fill="#EDF0FF"
+    />
+  </svg>
+)

src/components/icons/rendezvous/ArrowBackIcon.tsx
@@ -1,0 +1,15 @@
+export const ArrowBackIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="11"
+    height="11"
+    viewBox="0 0 11 11"
+    fill="none"
+    aria-hidden="true"
+  >
+    <path
+      d="M4.86193 0.195262C5.12228 -0.0650874 5.54429 -0.0650874 5.80464 0.195262C6.06499 0.455612 6.06499 0.877621 5.80464 1.13797L2.27599 4.66662H9.99995C10.3681 4.66662 10.6666 4.96509 10.6666 5.33328C10.6666 5.70147 10.3681 5.99995 9.99995 5.99995H2.27599L5.80464 9.5286C6.06499 9.78894 6.06499 10.211 5.80464 10.4713C5.54429 10.7317 5.12228 10.7317 4.86193 10.4713L0.195262 5.80464C-0.0650874 5.54429 -0.0650874 5.12228 0.195262 4.86193L4.86193 0.195262Z"
+      fill="#626A80"
+    />
+  </svg>
+)

commit 76a7e9da147ad3fda3ac9e2e709141db72ce91b4

🐛(grist) fix bug that prevented grist contact form submission
Fix a bug introduced in 8c90055 that forced every button to `button`
type, making the submit button used in the grist contact form not doing
anything anymore

src/components/ui-kit-v2/Button.tsx
@@ -31,6 +31,7 @@ interface ButtonProps {
   className?: string
   style?: React.CSSProperties
   onClick?: React.MouseEventHandler<HTMLButtonElement | HTMLAnchorElement>
+  type?: 'button' | 'submit' | 'reset'
 }
 
 const transition = 'transition ease-in-out delay-50 duration-300'
@@ -76,6 +77,7 @@ export const Button: React.FC<ButtonProps> = ({
   className,
   style,
   onClick,
+  type = 'button',
 }) => {
   const t = useTranslations()
   const normalizedVariant = (variant as string) || 'primary_brand'
@@ -152,7 +154,7 @@ export const Button: React.FC<ButtonProps> = ({
       aria-label={finalAriaLabel}
       style={inlineStyle}
       onClick={onClick}
-      type="button"
+      type={type}
     >
       {content}
     </button>

src/sections/produits/grist/Contact.tsx
@@ -128,7 +128,9 @@ export const Contact = () => {
             errors={errors.message}
           />
           <div className="w-full md:w-fit md:ml-auto md:mx-auto">
-            <Button fullWidth>{t('common.send')}</Button>
+            <Button fullWidth type="submit">
+              {t('common.send')}
+            </Button>
           </div>
         </form>
       ) : (