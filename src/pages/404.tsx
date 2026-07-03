b07d7587e59e4991a854ad03bee99dcf939cd783
Eléonore <95469923+elvoisin@users.noreply.github.com>
2026-07-03 18:13:21 +0200

    💄(404) update 404 page (#217)

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,8 +3,8 @@ import { useRouter } from 'next/router'
 import Link from 'next/link'
 import type { GetStaticProps } from 'next'
 import { Layout } from '@/components/Layout'
-import QuestionMark from '@/assets/question-mark.svg'
-import { Button } from '@/components/Button'
+import Image404 from '@/assets/404.svg'
+import { Button } from '@/components/ui-kit-v2/Button'
 import { useTranslations } from '@/locales/useTranslations'
 
 /**
@@ -21,11 +21,13 @@ export default function NotFound() {
   const { locale, defaultLocale, asPath } = useRouter()
   return (
     <Layout title="Page introuvable" background="gray">
-      <div className="text-center max-w-[26rem] m-auto px-4 py-16 md:py-24">
-        <Image src={QuestionMark} alt="" className="m-auto mb-8" />
-        <h1 className="text-3xl font-bold mb-8">{t('not_found_page.title')}</h1>
+      <div className="text-center max-w-[36rem] m-auto px-4 py-16 md:py-24">
+        <Image src={Image404} alt="Image 404" className="m-auto mb-8" />
+        <h1 className="text-3xl font-bold mb-4">{t('not_found_page.title')}</h1>
 
-        <p className="mb-8">{t('not_found_page.body')}</p>
+        <p className="text-gray-600 text-base mb-12">
+          {t('not_found_page.body')}
+        </p>
 
         {locale !== defaultLocale && (
           <p className="mb-8">
@@ -44,7 +46,14 @@ export default function NotFound() {
         )}
 
         <p className="flex justify-center">
-          <Button href="/">{t('common.back_to_home')}</Button>
+          <Button
+            href="/"
+            variant="secondary_brand"
+            target="_blank"
+            className="hidden lg:flex"
+          >
+            {t('common.back_to_home')}
+          </Button>
         </p>
       </div>
     </Layout>
